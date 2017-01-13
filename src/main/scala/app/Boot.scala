ddbdbf9c0104744132a94dbcbbe524fab020e92a
wuv1982 <wuv1982@gmail.com>
2017-01-14 02:06:02 +0900

    read http.port

--- src/main/scala/app/Boot.scala
+++ src/main/scala/app/Boot.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 
 import scala.io.StdIn
+import com.typesafe.config.ConfigFactory
 
 object Boot extends App {
   println("Start Web Server...")
@@ -15,9 +16,12 @@ object Boot extends App {
 
   val server = new WebServer()
 
-  val bindingFuture = Http().bindAndHandle(server.routes, "localhost", 8080)
+  val config = ConfigFactory.load
+  val port: Int = config.getInt("http.port")
 
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  val bindingFuture = Http().bindAndHandle(server.routes, "0.0.0.0", port)
+
+  println(s"Server online at http://localhost:${port}/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
